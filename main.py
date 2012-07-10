438e3e38291d810cccefc3197e2d80358cf8b098
Alejandro Molina <jalemolina@gmail.com>
2012-07-10 00:01:12 -0300

    Tenemos algo en el README

--- main.py
+++ main.py
@@ -9,31 +9,6 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 
 
-#class Preceptor(db.Model):
-#    nombre = db.StringProperty(required=True, multiline=False)
-#    apellido = db.StringProperty(required=True, multiline=False)
-#    dni = db.IntegerProperty()
-#    email = db.EmailProperty(required=True)
-
-
-#class Alumno(db.Model):
-#    nombre = db.StringProperty(required=True, multiline=False)
-#    apellido = db.StringProperty(required=True, multiline=False)
-#    dni = db.IntegerProperty(required=True)
-#    curso = db.IntegerProperty(required=True)
-#    division = db.StringProperty(required=True)
-#    asignatura = db.StringProperty(required=True, multiline=False)
-#    examen = db.StringProperty(required=True,
-#                                multiline=False,
-#                                choices=set(["LIBRES",
-#                                            "PREVIOS",
-#                                            "EQUIVALENCIAS",
-#                                            "COMPLEMENTARIOS",
-#                                            "REGULARES"]))
-#    fecha_insc = db.DateTimeProperty(auto_now_add=True)
-#    inscripto_por = db.UserProperty(required=True)
-
-
 class MainPage(webapp.RequestHandler):
     def get(self):
         if users.get_current_user():
@@ -49,27 +24,6 @@ class MainPage(webapp.RequestHandler):
             }
         path = os.path.join(os.path.dirname(__file__), "main.html")
         self.response.out.write(template.render(path, template_values))
-
-
-#class Preceptores(webapp.RequestHandler):
-#    def get(self):
-#        if users.get_current_user():
-#            estado = "Salir"
-#            diracceso = users.create_logout_url('/')
-#            plantilla = "preceptores.html"
-#        else:
-#            estado = "Acceder"
-#            diracceso = users.create_login_url('/')
-#            plantilla = "error.html"
-#        contenido = ""
-#        valores_plantilla = {
-#                            'usuario': users.get_current_user(),
-#                            'estado': estado,
-#                            'diracceso': diracceso,
-#            }
-#        valores_plantilla['contenido'] = contenido
-#        path = os.path.join(os.path.dirname(__file__), plantilla)
-#        self.response.out.write(template.render(path, valores_plantilla))
 
 
 application = webapp.WSGIApplication(
